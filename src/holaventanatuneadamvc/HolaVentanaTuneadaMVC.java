3900d6ba0cdce42a6572196a4c72ccd1c9140fb0
Sergio Teacher <Sergio Teacher@MINIWIN>
2025-03-11 11:38:52 +0100

    Iniciando el proyecto Hola tuneado.

---
+++ src/holaventanatuneadamvc/HolaVentanaTuneadaMVC.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package holaventanatuneadamvc;
+
+/**
+ * Clase principal del proyecto
+ * @author Sergio Teacher
+ */
+public class HolaVentanaTuneadaMVC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Iniciando HolaVentanaTuneadaMVC... ")
+;
+    }
+    
+}
